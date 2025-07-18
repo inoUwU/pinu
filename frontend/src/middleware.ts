49f12a5c490ec5b91daa448bddc6de24b9f8034c
ino <87593133+inoUwU@users.noreply.github.com>
2025-07-19 00:43:09 +0900

    feat(middleware): ✨ ミドルウェアを追加し、ルートリクエストをクライアントページにリダイレクト

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,10 @@
+import { type NextRequest, NextResponse } from 'next/server';
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL('/client', request.url));
+}
+
+export const config = {
+  matcher: '/',
+};
