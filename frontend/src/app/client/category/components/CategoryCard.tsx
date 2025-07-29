18e893eace5714ee9142eb39419abefea8b3d56a
ino <87593133+inoUwU@users.noreply.github.com>
2025-07-29 23:53:46 +0900

    feat(category): ✨ カテゴリーカードコンポーネントを追加し、ページに統合

---
+++ frontend/src/app/client/category/components/CategoryCard.tsx
@@ -0,0 +1,35 @@
+import { useRouter } from "next/navigation";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+
+export type CategoryCardProps = {
+  category: { name: string; menuId: number; imageUrl?: string };
+  showImage?: boolean;
+};
+
+export default function CategoryCard({
+  category,
+  showImage = false,
+}: CategoryCardProps) {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/client/menu/${category.menuId}`);
+  };
+
+  return (
+    <Card onClick={handleClick}>
+      <CardContent>
+        <div className='flex gap-2'>
+          {showImage && category.imageUrl && (
+            <img
+              src={category.imageUrl}
+              alt={category.name}
+              className='w-24 h-24 object-cover rounded-md mb-2'
+            />
+          )}
+          <CardTitle className='text-2xl break-all'>{category.name}</CardTitle>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
--- frontend/src/app/client/category/page.tsx
+++ frontend/src/app/client/category/page.tsx
@@ -1,8 +1,42 @@
+"use client";
+
+import CategoryCard from "@/app/client/category/components/CategoryCard";
+import PageHeader from "@/app/client/components/PageHeader";
+
+const CategorySection = () => {
+  return (
+    <div className='h-full w-full p-4 flex flex-col gap-2 justify-start overflow-auto'>
+      <CategoryCard
+        key={1}
+        category={{
+          name: "飲み物aaaaaaaaaaaaaaaaaaaaaaa",
+          imageUrl: "https://placehold.jp/150x150.png",
+          menuId: 1,
+        }}
+        showImage
+      />
+      <CategoryCard
+        key={2}
+        category={{
+          name: "食べ物",
+          imageUrl: "https://placehold.jp/150x150.png",
+          menuId: 2,
+        }}
+        showImage
+      />
+    </div>
+  );
+};
+
 export default function CategoryPage() {
   return (
-    <div>
-      <h1>カテゴリー</h1>
-      <p>ここにカテゴリーのコンテンツが表示されます。</p>
+    <div className='flex flex-col h-full'>
+      <div className='basis-1/12 flex-shrink-0 flex align-middle justify-center'>
+        <PageHeader title='カテゴリー' />
+      </div>
+      <div className='basis-11/12 grow overflow-auto bg-gray-50 pt-2'>
+        <CategorySection />
+      </div>
     </div>
   );
 }
